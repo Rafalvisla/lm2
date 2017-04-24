cab7370b0f588186cdbf89c012e1782e1898d2d8
Rafalvisla <kegriz@wp.pl>
2017-04-24 09:30:22 +0200

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -1,6 +1,1 @@
-$('#shop_order_form h2').each(function() {
-   $(this).html('TWOJE DANE');        
-});
-$('label[for=shop_order_phone]').each(function() {
-   $(this).html('Telefon (opcjonalnie)');        
-});
+
